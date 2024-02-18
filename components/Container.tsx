d3bd837e68407f4a046ea673a5c8400c666a82e5
joaoleandro <new.joaoleandro@gmail.com>
2024-02-18 10:47:24 -0300

    feat: organizing container padding

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,13 +1,9 @@
 interface ContainerProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-    return ( 
-        <div className="max-w-[1920px] mx-auto px-5 sm:px-36">
-            {children}
-        </div>
-     );
-}
- 
+  return <div className="max-w-[1920px] mx-auto px-5 md:px-20 xl:px-28 lg:px-36">{children}</div>;
+};
+
 export default Container;
